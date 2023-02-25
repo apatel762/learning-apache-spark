5e1dad2fcdc8eefaba136fb6ee03f150234cf95c
Arjun Patel <relay-git-1LKQ1GqLp0Nn4@aspatel.com>
2023-02-25 13:05:31 +0000

    begin keyword ranking practical

--- src/main/java/com/aspatel/KeywordRankingPractical.java
+++ src/main/java/com/aspatel/KeywordRankingPractical.java
@@ -1,16 +1,30 @@
 package com.aspatel;
 
+import java.util.Arrays;
+import java.util.Iterator;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaSparkContext;
 
+/**
+ * Given the input text file, representing the subtitles of a Docker course on Udemy: use Spark to
+ * process the data, and gather important keywords.
+ */
 public class KeywordRankingPractical {
 
   public static void main(String[] args) {
     SparkConf conf = new SparkConf().setAppName("keyword_ranking").setMaster("local[*]");
     try (JavaSparkContext sc = new JavaSparkContext(conf)) {
       sc.textFile("src/main/resources/subtitles/input.txt")
+          .flatMap(KeywordRankingPractical::splitLine)
+          .filter(StringUtils::isAlpha)
           .take(10)
-          .forEach(line -> System.out.println(line));
+          .forEach(System.out::println);
     }
+  }
+
+  private static Iterator<String> splitLine(String line) {
+    String[] words = line.split(" ");
+    return Arrays.stream(words).iterator();
   }
 }
