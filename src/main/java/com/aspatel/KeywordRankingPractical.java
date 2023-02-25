b1199a3d9261e7f96f649cf8b3f0c9544b677914
Arjun Patel <relay-git-1LKQ1GqLp0Nn4@aspatel.com>
2023-02-25 13:26:36 +0000

    order input words in descending order by occurrences

--- src/main/java/com/aspatel/KeywordRankingPractical.java
+++ src/main/java/com/aspatel/KeywordRankingPractical.java
@@ -2,9 +2,11 @@ package com.aspatel;
 
 import java.util.Arrays;
 import java.util.Iterator;
+import java.util.Locale;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaSparkContext;
+import scala.Tuple2;
 
 /**
  * Given the input text file, representing the subtitles of a Docker course on Udemy: use Spark to
@@ -18,9 +20,18 @@ public class KeywordRankingPractical {
       sc.textFile("src/main/resources/subtitles/input.txt")
           .flatMap(KeywordRankingPractical::splitLine)
           .filter(StringUtils::isAlpha)
+          .map(word -> word.toLowerCase(Locale.ENGLISH))
+          .mapToPair(word -> new Tuple2<>(word, 1L))
+          .reduceByKey(Long::sum)
+          .mapToPair(KeywordRankingPractical::flipTuple)
+          .sortByKey(false)
           .take(10)
           .forEach(System.out::println);
     }
+  }
+
+  private static <KEY, VALUE> Tuple2<VALUE, KEY> flipTuple(Tuple2<KEY, VALUE> pair) {
+    return new Tuple2<>(pair._2, pair._1);
   }
 
   private static Iterator<String> splitLine(String line) {
