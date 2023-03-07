3312ce683fe5ce6bfdaf2044b412aad0f7e3ecb2
Arjun Patel <relay-git-1LKQ1GqLp0Nn4@aspatel.com>
2023-03-07 21:00:38 +0000

    big data exercise 2 (wip)

--- src/main/java/com/aspatel/BigDataPractical.java
+++ src/main/java/com/aspatel/BigDataPractical.java
@@ -15,8 +15,14 @@ public class BigDataPractical {
     SparkConf conf = new SparkConf().setAppName("big_data").setMaster("local[*]");
 
     try (JavaSparkContext sc = new JavaSparkContext(conf)) {
-      // build an RDD containing a key of courseId together
-      // with the number of chapters on that course.
+      /*
+      EXERCISE 1
+      Build an RDD containing a key of courseId together with the number of chapters
+      on that course.
+      */
+      System.out.println("EXERCISE 1");
+      System.out.println("(no output)");
+
       JavaPairRDD<String, Long> courseIdAndChapterCount =
           sc.textFile("src/main/resources/viewing figures/chapters.csv")
               .mapToPair(
@@ -24,10 +30,80 @@ public class BigDataPractical {
                     String[] mapping = s.split(",");
                     return new Tuple2<>(mapping[1], 1L);
                   })
-              .reduceByKey(Long::sum)
-              .sortByKey();
-      courseIdAndChapterCount.foreach(
-          o -> System.out.println("Course " + o._1 + " has " + o._2 + " chapters"));
+              .reduceByKey(Long::sum);
+
+      /*
+      EXERCISE 2
+      Produce a ranking chart detailing which are the most popular courses by score.
+
+      We think that if a user sticks it through most of the course, that's more
+      deserving of "points" than if someone bails out just a quarter way through the
+      course. So we've cooked up the following scoring system:
+
+       - If a user watches more than 90% of the course, the course gets 10 points
+       - If a user watches > 50% but <90% , it scores 4
+       - If a user watches > 25% but < 50% it scores 2
+       - Less than 25% is no score
+      */
+      System.out.println();
+      System.out.println("EXERCISE 2");
+
+      JavaPairRDD<String, String> chaptersToUsers =
+          sc.textFile("src/main/resources/viewing figures/views-*.csv")
+              .mapToPair(
+                  line -> {
+                    String[] values = line.split(",");
+                    String userId = values[0];
+                    String chapterId = values[1];
+                    return new Tuple2<>(chapterId, userId);
+                  })
+              // a user may watch the same chapter multiple times, so must make
+              // sure that we are looking at distinct entries in the data set
+              .distinct();
+
+      JavaPairRDD<String, String> chaptersToCourses =
+          sc.textFile("src/main/resources/viewing figures/chapters.csv")
+              .mapToPair(
+                  line -> {
+                    String[] values = line.split(",");
+                    String chapterId = values[0];
+                    String courseId = values[1];
+                    return new Tuple2<>(chapterId, courseId);
+                  });
+
+      // TODO: how many chapters has the user seen in the course
+      //  -> we need to join chapter : course and reduce
+      chaptersToUsers
+          .join(chaptersToCourses)
+          .mapToPair(
+              row -> {
+                // Unpacking the joined row...
+                String userId = row._2._1;
+                String courseId = row._2._2;
+
+                // Think of the new data structure like this:
+                // "the [userId] has seen a [chapterId] from this [courseId]"
+                return new Tuple2<>(userId, new Tuple2<>(courseId, 1L));
+              });
+
+      /*
+      EXERCISE 3
+      This is optional but as a final touch, we can get the titles using a final
+      (small data) join. You can also sort by total score (descending) to get the
+      results in a pleasant format.
+      */
+      System.out.println();
+      System.out.println("EXERCISE 3");
+
+      /*
+      EXERCISE 4
+      Once you've got the output looking "nice", it's more fun to run the job against
+      some real data - change the value for testMode to false, and it will read the data
+      from disk (using some anonymized real viewing figures from
+      VirtualPairProgrammers.com, over a short period from last year).
+      */
+      System.out.println();
+      System.out.println("EXERCISE 4");
     }
   }
 
